f806f86e31da966853cc925951279fe0c99f2484
Trung Dung <trungdung6789a0@gmail.com>
2024-06-09 22:10:47 +0700

    adding video for EV

--- src/application/MainPage.java
+++ src/application/MainPage.java
@@ -132,7 +132,7 @@ public class MainPage extends Application {
 		    "Herpes Simplex Virus", "Herpesviridae", "DNA", "icosahedral",
 		    Arrays.asList("Human"), "contact", 2, "mild to severe", 0.001,
 		    new Image("file:D:/HUST ICT/VirusDemonstration/Virus_Demonstration/Enveloped/Herpes Simplex Virus (Herpesviridae).png"), 
-		    "file:D:/HUST ICT/VirusDemonstration/Virus_Demonstration/Enveloped/Herpes Simplex Virus (Herpesviridae).mp4",
+		    "EnvelopedVideo\\Herpes Simplex Virus.mp4",
 		    "lipid envelope with glycoprotein spikes (gB, gD, gH, gL)"
 		);
 		EVmenu.addEnvelopedVirus(herpesSimplexVirus);
@@ -141,7 +141,7 @@ public class MainPage extends Application {
 		    "Influenza Virus", "Orthomyxoviridae", "RNA", "segmented",
 		    Arrays.asList("Human", "Bird"), "airborne", 1, "moderate to severe", 0.01,
 		    new Image("file:D:\\HUST ICT\\VirusDemonstration\\Virus_Demonstration\\Enveloped\\Influena_virus.jpg"), 
-		    "file:D:/HUST ICT/VirusDemonstration/Virus_Demonstration/Enveloped/Influenza Virus (Orthomyxoviridae).mp4",
+		    "EnvelopedVideo\\Influenza Virus.mp4",
 		    "lipid envelope with hemagglutinin (HA) and neuraminidase (NA)"
 		);
 		EVmenu.addEnvelopedVirus(influenzaVirus);
@@ -150,7 +150,7 @@ public class MainPage extends Application {
 		    "HIV", "Retroviridae", "RNA", "complex",
 		    Arrays.asList("Human", "Primate"), "contact", 14, "severe", 0.003,
 		    new Image("file:D:\\HUST ICT\\VirusDemonstration\\Virus_Demonstration\\Enveloped\\HIV (Retroviridae).png"), 
-		    "file:D:/HUST ICT/VirusDemonstration/Virus_Demonstration/Enveloped/HIV (Retroviridae).mp4",
+		    "EnvelopedVideo\\HIV.mp4",
 		    "lipid envelope with glycoprotein spikes (gp120, gp41)"
 		);
 		EVmenu.addEnvelopedVirus(hiv);
@@ -159,7 +159,7 @@ public class MainPage extends Application {
 		    "Hepatitis B Virus", "Hepadnaviridae", "DNA", "icosahedral",
 		    Arrays.asList("Human"), "contact", 30, "mild to severe", 0.001,
 		    new Image("file:D:\\HUST ICT\\VirusDemonstration\\Virus_Demonstration\\Enveloped\\Hepatitis B Virus (Hepadnaviridae).jpeg"), 
-		    "file:D:/HUST ICT/VirusDemonstration/Virus_Demonstration/Enveloped/Hepatitis B Virus (Hepadnaviridae).mp4",
+		    "EnvelopedVideo\\Hepatitis B.mp4",
 		    "lipid envelope with surface antigen proteins"
 		);
 		EVmenu.addEnvelopedVirus(hepatitisB);
@@ -168,7 +168,7 @@ public class MainPage extends Application {
 		    "Varicella-Zoster Virus", "Herpesviridae", "DNA", "icosahedral",
 		    Arrays.asList("Human"), "contact", 14, "mild to moderate", 0.001,
 		    new Image("file:D:\\HUST ICT\\VirusDemonstration\\Virus_Demonstration\\Enveloped\\Varicella-Zoster Virus (Herpesviridae).jpg"), 
-		    "file:D:/HUST ICT/VirusDemonstration/Virus_Demonstration/Enveloped/Varicella-Zoster Virus (Herpesviridae).mp4",
+		    "EnvelopedVideo\\VZV.mp4",
 		    "lipid envelope with glycoprotein spikes (gC, gE, gH/gL)"
 		);
 		EVmenu.addEnvelopedVirus(varicellaZoster);
@@ -177,7 +177,7 @@ public class MainPage extends Application {
 		    "Hepatitis C Virus", "Flaviviridae", "RNA", "icosahedral",
 		    Arrays.asList("Human"), "contact", 56, "moderate to severe", 0.0005,
 		    new Image("file:D:\\HUST ICT\\VirusDemonstration\\Virus_Demonstration\\Enveloped\\Hepatitis C Virus (Flaviviridae).jpeg"), 
-		    "file:D:/HUST ICT/VirusDemonstration/Virus_Demonstration/Enveloped/Hepatitis C Virus (Flaviviridae).mp4",
+		    "EnvelopedVideo\\Hepatitis C.mp4",
 		    "lipid envelope with glycoprotein spikes (E1, E2)"
 		);
 		EVmenu.addEnvelopedVirus(hepatitisC);
@@ -186,7 +186,7 @@ public class MainPage extends Application {
 		    "Ebola Virus", "Filoviridae", "RNA", "helical",
 		    Arrays.asList("Human", "Bat"), "contact", 21, "severe", 0.002,
 		    new Image("file:D:\\HUST ICT\\VirusDemonstration\\Virus_Demonstration\\Enveloped\\Ebola Virus.png"), 
-		    "file:D:/HUST ICT/VirusDemonstration/Virus_Demonstration/Enveloped/Ebola Virus (Filoviridae).mp4",
+		    "EnvelopedVideo\\Ebola.mp4",
 		    "lipid envelope with glycoprotein spikes (GP1,2)"
 		);
 		EVmenu.addEnvelopedVirus(ebolaVirus);
@@ -204,7 +204,7 @@ public class MainPage extends Application {
 		    "SARS-CoV-2", "Coronaviridae", "RNA", "helical",
 		    Arrays.asList("Human"), "airborne", 5, "moderate to severe", 0.01,
 		    new Image("file:D:\\HUST ICT\\VirusDemonstration\\Virus_Demonstration\\Enveloped\\SARS-CoV-2 (Coronaviridae).jpeg"), 
-		    "file:D:/HUST ICT/VirusDemonstration/Virus_Demonstration/Enveloped/SARS-CoV-2 (Coronaviridae).mp4",
+		    "EnvelopedVideo\\Corona.mp4",
 		    "lipid envelope with spike glycoproteins (S proteins)"
 		);
 		EVmenu.addEnvelopedVirus(sarsCov2);
@@ -213,7 +213,7 @@ public class MainPage extends Application {
 		    "Measles Virus", "Paramyxoviridae", "RNA", "helical",
 		    Arrays.asList("Human"), "airborne", 10, "severe", 0.002,
 		    new Image("file:D:\\HUST ICT\\VirusDemonstration\\Virus_Demonstration\\Enveloped\\Measles Virus (Paramyxoviridae).jpeg"), 
-		    "file:D:/HUST ICT/VirusDemonstration/Virus_Demonstration/Enveloped/Measles Virus (Paramyxoviridae).mp4",
+		    "EnvelopedVideo\\Measles.mp4",
 		    "lipid envelope with hemagglutinin (H) and fusion (F) proteins"
 		);
 		EVmenu.addEnvelopedVirus(measlesVirus);
